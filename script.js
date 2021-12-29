267d7a738606c81b7a49ab143b70d0af3c337570
Nayal404 <93335142+Nayal404@users.noreply.github.com>
2021-12-29 18:01:47 +0545

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ function guess(){
     var Random_numbers = Math.floor(Math.random() * 10);
     if (userguess == Random_numbers){
         // console.log('You won');
-        msg = "You won";
-        document.getElementById('result').innerHTML = msg;
+//         msg = "You won";
+        document.getElementById('result').innerHTML = "Hurray!You guessed it right. The number was" +Random_numbers ;
     }
     else if (userguess != Random_numbers){
         // console.log('You lost')
         msg1 = "You lost"
-        document.getElementById('result').innerHTML = msg1;
+        document.getElementById('result').innerHTML = msg1+ "because the number was " +Random_numbers ;
     }
     else{
         // console.log('Invalid number')
