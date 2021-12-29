49bf3d0ce4287fbc458be00adaa18d6a32fd1535
Nayal404 <93335142+Nayal404@users.noreply.github.com>
2021-12-29 18:04:26 +0545

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ function guess(){
     if (userguess == Random_numbers){
         // console.log('You won');
 //         msg = "You won";
-        document.getElementById('result').innerHTML = "Hurray!You guessed it right. The number was" +Random_numbers ;
+        document.getElementById('result').innerHTML = "Hurray!You guessed it right. The number was " +Random_numbers ;
     }
     else if (userguess != Random_numbers){
         // console.log('You lost')
         msg1 = "You lost"
-        document.getElementById('result').innerHTML = msg1+ "because the number was " +Random_numbers ;
+        document.getElementById('result').innerHTML = msg1+ " because the number was " +Random_numbers ;
     }
     else{
         // console.log('Invalid number')
