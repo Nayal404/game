4470032700aadd8d3a25369b10a4753ebcbb23de
Nayal404 <imnayal404@gmail.com>
2022-07-17 15:15:39 +0545

    Updated! v2.0

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-function guess(){
-    var userguess = document.getElementById('userguess').value;
-    var Random_numbers = Math.floor(Math.random() * 10);
-    if (userguess == Random_numbers){
-        // console.log('You won');
-//         msg = "You won";
-        document.getElementById('result').innerHTML = "Hurray!You guessed it right. The number was " +Random_numbers ;
-    }
-    else if (userguess != Random_numbers){
-        // console.log('You lost')
-        msg1 = "You lost"
-        document.getElementById('result').innerHTML = msg1+ " because the number was " +Random_numbers ;
-    }
-    else{
-        // console.log('Invalid number')
-        msg2 = "The number you just typed is out of my mined! So try guessing another number ok!?"
-        document.getElementById('result').innerHTML = msg2;
-    }
+console.log('JavaScript Executed Successfully!!!')
+let btn = document.querySelector('button');
+const userGuess = document.getElementById('userGuess').value;
+btn.onclick = ()=>{
+    addEventListener('click', function game(){
+        const randomNumbers = Math.floor(Math.random()*10)
+        // console.log(randomNumbers);
+        if (userGuess > randomNumbers){
+            document.getElementById('msgBox').innerHTML = "Too high number than the real one! Try Another"
+        }
+        else if (userGuess < randomNumbers){
+            document.getElementById('msgBox').innerHTML = 'Too less number than the real one! Try Another'
+        }
+        else if (userGuess == randomNumbers){
+            document.getElementById('msgBox').innerHTML = 'Congrats! You Got the right one. You won!!!'
+            /*setInterval(() => {
+                //Just for fun!!!
+                alert('Congrats! You Successfully Won and guessed the right number!')
+            }, 1000);*/
+
+        }
+    })
 }
